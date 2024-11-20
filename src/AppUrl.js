50d44d4d89f1932f6ed54f1a175cb2a606f9ce45
Shamim Sheikh <shamimsheikh@Shamims-MacBook.local>
2024-11-20 20:34:44 +0530

    updated

--- src/AppUrl.js
+++ src/AppUrl.js
@@ -1,5 +1,5 @@
-// export const ApiUrl = 'https://api.iraconquiz.in/api'
-export const ApiUrl = 'http://localhost:5001/api'
+export const ApiUrl = 'https://api.iraconquiz.in/api'
+// export const ApiUrl = 'http://localhost:5001/api'
 
 export const ImgUrl =  'https://api.iraconquiz.in'
 // export const ImgUrl = 'http://localhost:3000'
